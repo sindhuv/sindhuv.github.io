7a00c2a0102dc70431ff426ede0c5c9771451009
sindhuv <vr.sindhu@gmail.com>
2015-11-16 17:31:17 +0530

    Simple crawler through wikipedia(en) pages and dumper complete so far

---
+++ SearchNRank/wikipedia_schema.sql
@@ -0,0 +1,7 @@
+use wikipedia;
+drop table if exists urls;
+create table urls (id int AUTO_INCREMENT primary key, url nvarchar(1000)) COLLATE utf8_bin;
+alter table urls add unique index url_index (`url`);
+drop table if exists words;
+create table words(id int AUTO_INCREMENT primary key, word nvarchar(1000)) collate utf8_bin;
+alter table words add unique index word_index (`word`);
